b41924180e1931a0018f31b44f58dd3873c818e5
VikasVats1998 <vvats888@gmail.com>
2022-10-03 06:30:14 -0700

    initial

---
+++ DataTypes/numberFunction.js
@@ -0,0 +1,25 @@
+console.clear();
+
+var num1=10;
+
+// toString() number as input and returns a string
+console.log(num1.toString());
+
+
+var strNum1="10";
+var strNum2="11.9";
+var strNum3="88.3456762";
+// parseInt() string numeral as input and returns a number
+console.log(parseInt(strNum1));
+console.log(parseInt(strNum2));
+
+//parseFloat() string numeral as input and returns a number
+console.log(parseFloat(strNum2));
+
+// We cann't change string numeral to integer or floating number
+
+
+// toFixed() takes floating number and rounds it off to given position
+// console.log(strNum3.toFixed());
+// console.log(strNum3.toFixed(3));
+// console.log(strNum3.toFixed(5));
---
+++ DataTypes/numbers.js
@@ -0,0 +1,11 @@
+console.clear();
+var num1 = 20;
+var num2 = 200;
+var num3 = 120.3;
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(num3));
+
+var diff=num2-num3;
+console.log(diff);
