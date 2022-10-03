5f927a147eb5fd0e94f2cc2dde927b503981e04a
VikasVats1998 <vvats888@gmail.com>
2022-10-03 06:30:26 -0700

    initial

---
+++ JavaScriptBasic/variable.js
@@ -0,0 +1,27 @@
+console.clear();
+var num1=20;
+console.log(num1);
+var num2=30;
+var sum=num1+num2;
+console.log(sum);
+
+// For string Variable
+
+var firstName="Vikas";
+var lastName="Vats";
+var string="Hello! "+firstName+" "+lastName;
+console.log(string);
+
+// For Different Cases
+
+// camelCase
+var firstName="Vikas";
+console.log(firstName);
+
+// For PascalCase
+var LastName="Vats";
+console.log(LastName);
+
+// snake_case
+var complete_name="Vikas Vats";
+console.log(complete_name);
